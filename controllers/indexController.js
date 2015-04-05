377761671e3a5df858a5103e73bb69796f67f690
jvee <jvee99@gmail.com>
2015-04-05 05:13:53 +0300

    Реализован поиск фильма по id

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,9 +1,27 @@
 var yate = require('yate');
+var kinopoisk = require('node-kinopoisk-ru');
 
 module.exports = function (req, res) {
 	var data = {data: {page: 'index'}};
 	var template = './pages/index.yate';
-	var renderResult = yate.run(template, null, data);
+	var renderResult;
+	
+	var id = req.query.id;
 
-	res.send(renderResult);
+	if (!id) {
+		renderResult = yate.run(template, null, data);
+		return res.send(renderResult);
+	}
+
+	kinopoisk.getById(id, null, function (error, result) {
+		if (error) {
+			renderResult = yate.run(template, null, data);
+			return res.send(renderResult);
+		}
+
+		data.data.film = result;
+
+		renderResult = yate.run(template, null, data);
+		res.send(renderResult);
+	});
 };
