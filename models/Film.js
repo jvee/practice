5108eee6afc5ac5c471293ae2969dfd74e3dde18
jvee <jvee99@gmail.com>
2015-04-06 01:05:22 +0300

    Реализован метод Film.getById

--- models/Film.js
+++ models/Film.js
@@ -1,24 +1,49 @@
 var mongoose = require('mongoose');
+var kinopoisk = require('node-kinopoisk-ru');
+
 var Schema = mongoose.Schema;
 
 var filmShema = Schema({
   id: Number,
-  name: String,
+  title: String,
   rating: Number
 });
 
 filmShema.set('autoIndex', false);
 
-filmShema.pre('save', function (id, next) {
-	// validate id ?
-	// fetch
-		// if error
-			// parse error
-				// delete 
-		// update
-});
+filmShema.statics.getById = function (id, callback) {
+	if (!callback) {
+		return;
+	}
+
+	if (!id) {
+		callback(new Error('No ID'));
+	}
+
+	// TODO: check id type
+
+	Film.findOne({id: id}, function (error, film) {
+		if (error) {
+			return callback(error);
+		}
+
+		if (film) {
+			return callback(null, film);
+		}
+
+		kinopoisk.getById(id, null, function (error, filmData) {
+			if (error) {
+				return callback(error);
+			}
+
+			Film
+				.create(filmData)
+				.then(callback.bind(null, null), callback);
+		});
+	});
+
+};
 
 var Film = mongoose.model('Film', filmShema);
-
 
 module.exports = Film;
