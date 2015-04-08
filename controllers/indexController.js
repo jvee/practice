794c35b6c9e228237909f25a02227d313f8abf6f
jvee <jvee99@gmail.com>
2015-04-08 17:12:36 +0300

    Чистка контроллера главной

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,22 +2,19 @@ var Film = require('../orm/').collections.film;
 
 module.exports = function (req, res) {
 	var data = {page: 'index'};
-	var template = './pages/index.yate';
-	var renderResult;
-	
 	var id = req.query.id;
 
 	if (!id) {
-		return res.render('index.yate', data);
+		return res.render('index', data);
 	}
 
 	Film.findOrCreate({id: id}, {id:id}, function (error, result) {
 		if (error) {
-			return res.render('index.yate', data);
+			return res.render('index', data);
 		}
 
 		data.film = result;
 
-		res.render('index.yate', data);
+		res.render('index', data);
 	});
 };
