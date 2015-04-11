e4288c9e58dca65bc4167fde5f5c49edf3b03b68
jvee <jvee99@gmail.com>
2015-04-11 23:24:48 +0300

    Функционал обновления фильма

--- controllers/filmController.js
+++ controllers/filmController.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var controller = express();
 
 var Film = require('../orm/').collections.film;
 
 controller.set('views', __dirname + '/../pages');
+controller.use(bodyParser.urlencoded({ extended: true }));
 
 controller.use(function (req, res, next) {
 	res.locals.page = 'Film';
@@ -34,6 +36,22 @@ controller.get('/:film_id', function (req, res) {
 
 controller.post('/:film_id/delete', function (req, res) {
 	Film.destroy(req.params.film_id, function (err) {
+		res.redirect('/film');
+	});
+});
+
+controller.get('/:film_id/edit', function (req, res) {
+	Film.findOne(req.params.film_id, function (err, film) {
+		if (err) {
+			return res.redirect('/');
+		}
+
+		res.render('film-edit', {film: film});
+	});
+});
+
+controller.post('/:film_id', function (req, res) {
+	Film.update(req.body.film.id, req.body.film, function (err) {
 		res.redirect('/film');
 	});
 });
