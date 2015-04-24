41e05922b4c158a627fd47b9e2a7b133fb98bcce
jvee <jvee99@gmail.com>
2015-04-24 13:22:08 +0300

    Рефакторинг authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,59 +7,18 @@ var User = require('../orm/').collections.user;
 controller.set('views', __dirname + '/../pages');
 controller.use(bodyParser.urlencoded({ extended: true }));
 
-controller.get('/login', function (req, res) {
-	if (req.session.user) {
-		return res.redirect('/');
-	}
+/**
+ * Controller routs
+ */
 
-	// TODO: вынести в dataSetup
-	res.locals.page = 'Login';
-	res.locals.form = {};
+controller
+	.all('/login', loginDataSetup)
+	.get('/login', loginRestrict, render('login'))
+	.post('/login', loginCheck, redirect('/'))
+	// TODO: оставить только POST для logout
+	.all('/logout', logout, redirect('/'));
 
-	res.render('login');
-});
 
-controller.post('/login', function (req, res, next) {
-	// TODO: вынести в dataSetup
-	res.locals.page = 'Login';
-
-	var formData = req.body;
-
-	res.locals.form = {
-		login: formData.login,
-		password: formData.password
-	};
-
-	User.check(formData, function (err, user) {
-		if (err) {
-			// TODO: move to constans texts
-			if (typeof err === 'string') {
-				res.locals.form.message = 'Wrong Login or Password';
-			} else {
-				next(err);
-			}
-			return res.render('login');
-		}
-
-		req.session.regenerate(function () {
-			console.log('[Auth]: logined user %s', user.login);
-			req.session.user = user;
-			res.redirect('/');
-		});
-
-	});
-});
-
-// TODO: оставить только POST для logout
-controller.all('/logout', function (req, res, next) {
-	req.session.destroy(function (err) {
-		if (err) {
-			next(err);
-		}
-
-		res.redirect('/');
-	});
-});
 
 controller.get('/signup', function (req, res) {
 	// res.render('signup');
@@ -82,5 +41,74 @@ controller.post('/signup', function (req, res) {
 	// 	});
 	// });
 });
+
+/**
+ * Middleware methods
+ */
+
+function loginDataSetup(req, res, next) {
+	res.locals.page = 'Login';
+	res.locals.form = {};
+	next();
+}
+
+function loginRestrict(req, res, next) {
+	if (req.session.user) {
+		return res.redirect('/');
+	}
+
+	next();
+}
+
+function loginCheck(req, res, next) {
+	var formData = req.body;
+
+	res.locals.form.login = formData.login;
+	res.locals.form.password = formData.password;
+
+	User.check(formData, function (err, user) {
+		if (err) {
+			// TODO: move to constans texts
+			if (typeof err === 'string') {
+				res.locals.form.message = 'Wrong Login or Password';
+			} else {
+				next(err);
+			}
+			return res.render('login');
+		}
+
+		req.session.regenerate(function () {
+			console.log('[Auth]: logined user %s', user.login);
+			req.session.user = user;
+			next();
+		});
+	});
+}
+
+function logout(req, res, next) {
+	req.session.destroy(function (err) {
+		if (err) {
+			next(err);
+		}
+
+		next();
+	});
+}
+
+/**
+ * Middleware helpers
+ */
+
+function render(template) {
+	return function (req, res) {
+		res.render(template);
+	};
+}
+
+function redirect(path) {
+	return function (req, res) {
+		res.redirect(path);
+	};
+}
 
 module.exports = controller;
