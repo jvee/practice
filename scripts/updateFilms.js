a6c16c0e36afcc2ff0e091d44c89fa8c1032c22e
jvee <jvee99@gmail.com>
2015-05-13 18:23:51 +0300

    Скрипт обновления информации по  фильмам в базе

---
+++ scripts/updateFilms.js
@@ -0,0 +1,67 @@
+var orm = require('../orm/');
+var kinopoisk = require('kinopoisk-ru');
+
+var filmsQueue;
+var filmsQueueInitialLengh;
+var notFoundRE = /Film with id \d{3,10} not found/;
+
+// TODO: films ids via script arguments
+// TODO: timeout via script arguments
+// TODO: film.update instance method
+// TODO: log error in then end ?
+// TODO: export films (ids) script
+// TODO: export watchlist (with user id via arguments)
+// TODO: import with stategies (merge | clean)
+
+orm.init(function (err, orm) {
+	var Film = orm.collections.film;
+
+	Film.find(function (err, films) {
+		filmsQueue = films;
+		filmsQueueInitialLengh = films.length;
+		update();
+	});
+});
+
+function update() {
+	var film = filmsQueue.shift();
+	var progress;
+
+	if (!film) {
+		console.log('[Updating]: Done');
+		return;
+	}
+
+	progress = ((1 - (filmsQueue.length + 1) / filmsQueueInitialLengh) * 100).toFixed(2);
+
+	console.log('[Updating]: %s (%d%)', film.title, progress);
+
+	kinopoisk.getById(film.id, null, function (err, filmData) {
+		if (err) {
+			if (err.message.match(notFoundRE)) {
+				console.warn('[Updating]: Error, %s', err.message);
+				return setTimeout(update, 1000);
+			}
+
+			throw new Error(err);
+		}
+
+		Object.keys(filmData).forEach(function (key) {
+			film[key] = filmData[key];
+		});
+
+		film.poster = '//www.kinopoisk.ru/images/film_big/' + film.id + '.jpg';
+
+		if (!film.rating) {
+			delete film.rating;
+		}
+
+		film.save(function (err, film) {
+			if (err) {
+				throw new Error(err);
+			}
+
+			setTimeout(update, 1000);
+		});
+	});
+}
